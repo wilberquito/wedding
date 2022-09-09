3552c30818fc668ae1db57efd0c9f0b726f3e42a
wilberquito <wbq.software@gmail.com>
2022-09-09 11:17:31 +0200

    implements countdown

--- src/CountDown.svelte
+++ src/CountDown.svelte
@@ -1,31 +1,5 @@
 <script>
-    import { pipe } from "./utils/Pipe";
-
     export const weddingDate = new Date("2022-09-09 17:15:30");
-
-    const MILISECONDS_TO_DAYS = 1000 * 60 * 60 * 24;
-    const MILISECONDS_TO_HOURS = MILISECONDS_TO_DAYS / 24;
-    const MILISECONDS_TO_MINUTS = MILISECONDS_TO_HOURS / 60;
-    const MILISECONDS_TO_SECONDS = MILISECONDS_TO_MINUTS / 60;
-
-    const addDays = (days) => (date) => {
-        const timestamp = date.setDate(date.getDate() + days);
-        return new Date(timestamp);
-    };
-
-    const addHours = (hours) => (date) => {
-        const timestamp = date.setTime(
-            date.getTime() + hours * MILISECONDS_TO_HOURS
-        );
-        return new Date(timestamp);
-    };
-
-    const addMinuts = (minuts) => (date) => {
-        const timestamp = date.setMinutes(date.getMinutes() + minuts);
-        return new Date(timestamp);
-    };
-
-    const diffMilliseconds = (d1) => (d2) => Math.abs(d1 - d2);
 
     let remainingDays;
     let remainingHours;
@@ -35,70 +9,86 @@
     updateRemainings();
     setInterval(updateRemainings, 1000);
 
-    function computeRemainingDays(miliseconds) {
-        return Math.ceil(miliseconds / MILISECONDS_TO_DAYS);
-    }
-
-    function computeRemainingHours(miliseconds) {
-        return Math.round(miliseconds / MILISECONDS_TO_HOURS);
-    }
-
-    function computeRemainingMinuts(miliseconds) {
-        return Math.round(miliseconds / MILISECONDS_TO_MINUTS);
-    }
-
-    function computeRemainingSeconds(miliseconds) {
-        return Math.round(miliseconds / MILISECONDS_TO_SECONDS);
-    }
-
     function computeRemainings() {
-        const currentDate = new Date();
-
-        const days = pipe(
-            diffMilliseconds(weddingDate),
-            computeRemainingDays
-        )(currentDate);
-
-        const hours = pipe(
-            addDays(days - 1),
-            diffMilliseconds(weddingDate),
-            computeRemainingHours
-        )(currentDate);
-
-        const minuts = pipe(
-            addDays(days - 1),
-            addHours(hours - 1),
-            diffMilliseconds(weddingDate),
-            computeRemainingMinuts
-        )(currentDate);
-
-        const seconds = pipe(
-            addDays(days - 1),
-            addHours(hours - 1),
-            addMinuts(minuts - 1),
-            diffMilliseconds(weddingDate),
-            computeRemainingSeconds
-        )(currentDate);
+        const countDownDate = weddingDate.getTime();
+        const now = new Date().getTime();
+        const distance = Math.abs(countDownDate - now);
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor(
+            (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         return {
             days,
             hours,
-            minuts,
+            minutes,
             seconds,
         };
     }
 
     function updateRemainings() {
-        let { days, hours, minuts, seconds } = computeRemainings();
+        let { days, hours, minutes, seconds } = computeRemainings();
         remainingDays = days;
         remainingHours = hours;
-        remainingMinuts = minuts;
+        remainingMinuts = minutes;
         remainingSeconds = seconds;
+    }
+
+    function zeroPad(num, pads = 2) {
+        return String(num).padStart(pads, "0");
     }
 </script>
 
-{remainingDays}
-{remainingHours}
-{remainingMinuts}
-{remainingSeconds}
-<!-- https://www.w3schools.com/howto/howto_js_countdown.asp -->
+<div class="countdown__container">
+    <div class="countdown__item">
+        <span class="countdown">
+            {zeroPad(remainingDays)}
+        </span>
+        <span class="label">días</span>
+    </div>
+    <div class="countdown__item">
+        <span class="countdown">
+            {zeroPad(remainingHours)}
+        </span>
+        <span class="label">horas</span>
+    </div>
+    <div class="countdown__item">
+        <span class="countdown">
+            {zeroPad(remainingMinuts)}
+        </span>
+        <span class="label">minutos</span>
+    </div>
+    <div class="countdown__item">
+        <span class="countdown">
+            {zeroPad(remainingSeconds)}
+        </span>
+        <span class="label">segundos</span>
+    </div>
+</div>
+
+<style>
+    .countdown__container {
+        background-color: rgb(191, 189, 189);
+        padding: 1rem;
+        width: calc(100% - 2rem);
+        height: 10rem;
+        display: flex;
+        align-items: center;
+    }
+    .countdown__item {
+        height: 100%;
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+    }
+    .countdown__item .countdown {
+        font-size: clamp(3rem, 2.5vw, 5rem);
+    }
+    .countdown__item .label {
+        font-size: clamp(1rem, 1vw, 1.5rem);
+    }
+</style>
