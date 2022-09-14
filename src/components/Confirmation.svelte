0acbb0475639422feca1560c5f03470ce9c00b83
wilberquito <wbq.software@gmail.com>
2022-09-14 23:40:59 +0200

    confirmation component uses data.json

--- src/components/Confirmation.svelte
+++ src/components/Confirmation.svelte
@@ -1,18 +1,22 @@
 <script>
     import { openNewWindow } from "./../utils";
+        import {data} from './../assets/data.json'
+        console.log(data['confirmation'])
 
-        export const title = "Confirmar Asistencia";
-        export const warnConfirmation = "Confirma antes del 31 de mayo";
-        export const confirmationUrl = "https://forms.gle/yw9XCV5awPuWxrVN8";
+        const {
+                googleDocs,
+                title,
+                description
+            } = data['confirmation']
 
         function openConfirmationGoogleForms() {
-                openNewWindow(confirmationUrl);
+                openNewWindow(googleAddress);
             }
 </script>
 
 <div class="g__section g__custom">
     <h1>{title}</h1>
-    <p>{warnConfirmation}</p>
+    <p>{description}</p>
     <button class="g__custom" on:click={openConfirmationGoogleForms}>
         confirmar
     </button>
