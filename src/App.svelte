8a8a59a9a0b03ea67211527815b2980f4f80e742
wilberquito <wbq.software@gmail.com>
2022-09-16 23:19:35 +0200

    even elements has different style

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,28 @@
 <script>
-	import CountDown from "./components/CountDown.svelte";
-	import Location from "./components/Location.svelte";
-	import Confirmation from "./components/Confirmation.svelte";
-	import Story from "./components/Story.svelte";
+    import { data } from './assets/data.json';
+        import CountDown from "./components/CountDown.svelte";
+        import Location from "./components/Location.svelte";
+        import Confirmation from "./components/Confirmation.svelte";
+        import Story from "./components/Story.svelte";
+        import BasicSection from './components/BasicSection.svelte'
 
+        const {
+                confirmation,
+                location
+            } = data;
+        console.log(confirmation, location) 
 </script>
 
 <CountDown />
-<Location />
-<Confirmation />
-<Story /> 
+<!-- location -->
+<BasicSection 
+    titleImg="images/palm.png"
+    title={location.title}
+    description={location.description} 
+    buttonText="ver mapa" 
+    buttonExternalUrl={location.googleAddress} />
+    <Confirmation />
+    <Story /> 
+
+    <BasicSection description="hola"/>
 
---
+++ src/components/BasicSection.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { openNewWindow } from "./../utils";
+        export let titleImg;
+        export let title;
+        export let description;
+        export let buttonText;
+        export let buttonExternalUrl;
+
+        const openExternalUrl = () => openNewWindow(externalUrl)
+</script>
+
+<div class="g__section">
+    {#if titleImg}
+        <img
+                src="{titleImg}"
+                alt="title img"
+                />
+
+    {/if}
+    <h1>{title}</h1>
+    <p class="text">{description}</p>
+    <button class="g__button" on:click={openExternalUrl}>
+        {buttonText}
+    </button>
+</div>
+
+<style>
+    img {
+        width: 300px;
+    }
+</style>
